0dbce000938895d3647882b40eab44f1c74b9382
Elif <elifsirin42@gmail.com>
2022-08-20 14:02:58 +0300

    Updated component state for dynamiclly app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import  { Component } from 'react';
+import { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
 
-  render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    this.state = {
+      name: "Elif",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name}</p>
+          <button>Change Name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

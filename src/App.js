7ac966c298d37a2dc69b4b19da75fe89a946732f
Elif <elifsirin42@gmail.com>
2022-08-21 13:09:17 +0300

    Added keys for mapping

--- src/App.js
+++ src/App.js
@@ -11,15 +11,19 @@ class App extends Component {
       monsters: [
         {
           name: "Linda",
+          id: "12e1231e",
         },
         {
           name: "Frank",
+          id: "12e1232e",
         },
         {
           name: "Jacky",
+          id: "12e1233e",
         },
         {
           name: "Elif",
+          id: "12e1234e",
         },
       ],
     };
@@ -29,7 +33,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
     );
